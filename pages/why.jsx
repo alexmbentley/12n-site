f885395fe9d1eb6e1f46a0efea587dd3f8aac504
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2023-03-17 14:44:56 +0000

    backup fonts added

--- pages/why.jsx
+++ pages/why.jsx
@@ -5,7 +5,116 @@ const why = () => {
   return (
     <div className={styles['container']}>
       <h2 className={styles['container__heading']}>Why join us?</h2>
-      <div className={styles['container__box']}></div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
+      <div className={styles['container__box']}>
+        <h5 className={styles['container__box--heading']}>
+          Join a supportive community
+        </h5>
+        <p className={styles['container__box--text']}>
+          Our group values community and our members are always willing to help
+          each other, regardless of their level of experience. As a new member,
+          you'll be welcomed with open arms and given the support you need to
+          succeed.
+        </p>
+      </div>
     </div>
   );
 };
